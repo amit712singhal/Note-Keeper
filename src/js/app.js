9303deb79f38ce10d0773194606e87ebc817e510
amit712singhal <rakshit.singhal712@gmail.com>
2024-08-24 18:29:18 +0530

    chore: Add toggle functionality for sidebar in small screens

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,17 @@
  */
 
 'use strict';
+
+// Importing the modules
+import { addEventOnElements } from './utils.js';
+
+// Toggle sidebar in small screen
+
+const /** {HTMLElement} */ $sidebar = document.querySelector( '[data-sidebar]' );
+const /** {Array<HTMLElement>} */ $sidebarTogglers = document.querySelectorAll( '[data-sidebar-toggler]' );
+const /** {HTMLElement} */ $overlay = document.querySelector( '[data-sidebar-overlay]' );
+
+addEventOnElements( $sidebarTogglers, 'click', () => {
+    $sidebar.classList.toggle( 'active' );
+    $overlay.classList.toggle( 'active' );
+} );
--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,20 @@
  */
 
 'use strict';
+
+/**
+ *
+ * @param {Array<HTMLElement>} $elements
+ * @param {string} event
+ * @param {Function} callback
+ */
+
+const addEventOnElements = function ( $elements, event, callback )
+{
+    $elements.forEach($element => {
+        $element.addEventListener(event, callback);
+    });
+
+}
+
+export { addEventOnElements };
