76b3c7fa4b2d05ef53e065adf4760e757f304bf5
amit712singhal <rakshit.singhal712@gmail.com>
2024-08-25 00:22:51 +0530

    feat: Add toggle functionality for theme switcher button

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,3 +3,30 @@
  */
 
 'use strict';
+
+const toggleTheme = function ()
+{
+    const /** {string} */ currentTheme = document.documentElement.getAttribute( 'data-theme' ) || 'light';
+    const /** {string} */ newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute( 'data-theme', newTheme );
+    localStorage.setItem( 'theme', newTheme );
+}
+
+// Initialize the theme
+
+const /** {string | null} */ storedTheme = localStorage.getItem( 'theme' );
+const /** {Boolean} */ systemThemeIsDark = window.matchMedia( '(prefers-color-scheme: dark)' ).matches;
+const /** {string} */ initialTheme = storedTheme || ( systemThemeIsDark ? 'dark' : 'light' );
+
+document.documentElement.setAttribute( 'data-theme', initialTheme );
+
+// Toggle theme
+window.addEventListener('DOMContentLoaded', function ()
+{
+  const /** {HTMLElement} */ $themeBtn = document.querySelector( '[data-theme-btn]' );
+  if( $themeBtn )
+  {
+    $themeBtn.addEventListener( 'click', toggleTheme );
+  }
+} );
