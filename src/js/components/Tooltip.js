cccd6a84dca1d93b36f3dda2059dcef34c9ebd1b
amit712singhal <rakshit.singhal712@gmail.com>
2024-09-06 13:55:03 +0530

    feat: Add Tooltip component for displaying tooltips on hover

--- src/js/components/Tooltip.js
+++ src/js/components/Tooltip.js
@@ -3,3 +3,34 @@
  */
 
 'use strict';
+
+/**
+ *
+ * @param {HTMLElement} $element
+ */
+
+
+export const Tooltip = function ( $element )
+{
+  const /** {HTMLElement} */ $tooltip = document.createElement( 'span' );
+  $tooltip.classList.add( 'tooltip', 'text-body-small' );
+
+  $element.addEventListener( 'mouseenter', function ()
+  {
+    $tooltip.textContent = this.dataset.tooltip;
+
+    const {
+      top,
+      left,
+      width,
+      height
+    } = this.getBoundingClientRect();
+
+    $tooltip.style.top = top + height + 4 + 'px';
+    $tooltip.style.left = left + width / 2 + 'px';
+    $tooltip.style.transform = 'translateX(-50%, 0)';
+    document.body.appendChild( $tooltip );
+  } );
+
+  $element.addEventListener( 'mouseleave', $tooltip.remove.bind( $tooltip ) );
+};
