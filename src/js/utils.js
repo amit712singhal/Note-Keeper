e788f62218415c45cfeac887f3d1a50d38b92966
amit712singhal <rakshit.singhal712@gmail.com>
2024-09-06 15:00:06 +0530

    refactor: Refactor utils.js for improved code organization and readability

--- src/js/utils.js
+++ src/js/utils.js
@@ -13,9 +13,10 @@
 
 const addEventOnElements = function ( $elements, event, callback )
 {
-    $elements.forEach($element => {
-        $element.addEventListener(event, callback);
-    });
+  $elements.forEach( $element =>
+  {
+    $element.addEventListener( event, callback );
+  } );
 
 }
 
@@ -25,14 +26,52 @@ const addEventOnElements = function ( $elements, event, callback )
  * @returns {string}
  */
 
-const getGreetingMsg = function (currentHour) {
+const getGreetingMsg = function ( currentHour )
+{
   const /** {string} */ greeting =
     currentHour < 5 ? 'Night' :
       currentHour < 12 ? 'Morning' :
         currentHour < 15 ? 'Noon' :
           currentHour < 17 ? 'Afternoon' :
             currentHour < 20 ? 'Evening' : 'Night';
-  return `Good ${greeting}`;
+  return `Good ${ greeting }`;
 }
 
-export { addEventOnElements , getGreetingMsg };
+let /** {HTMLElement | undefined} */ $lastActiveNavItem;
+
+/**
+ * Active nav item & deactivate last active nav item
+ */
+const activeNotebook = function ()
+{
+  if ( $lastActiveNavItem )
+  {
+    $lastActiveNavItem.classList.remove( 'active' );
+  }
+  this.classList.add( 'active' ); // this: $navItem
+  $lastActiveNavItem = this; // this: $navItem
+}
+
+/**
+ * @param {HTMLElement} $element
+ */
+const makeElemEditable = function ($element)
+{
+  $element.setAttribute('contenteditable', 'true');
+  $element.focus();
+  document.execCommand('selectAll', false, null);
+  $element.addEventListener('keydown', function (e)
+  {
+    if (e.key === 'Enter')
+    {
+      e.preventDefault();
+      this.blur();
+    }
+  });
+  $element.addEventListener('blur', function ()
+  {
+    this.removeAttribute('contenteditable');
+  });
+}
+
+export { addEventOnElements, getGreetingMsg, activeNotebook, makeElemEditable };
