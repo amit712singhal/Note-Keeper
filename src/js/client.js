5409882c2a212cc5554a98d7d5af647255cb668d
amit712singhal <rakshit.singhal712@gmail.com>
2024-09-08 14:19:26 +0530

    refactor: Refactor client.js for improved code organization and readability

--- src/js/client.js
+++ src/js/client.js
@@ -3,3 +3,50 @@
  */
 
 'use strict';
+
+import { NavItem } from './components/NavItem.js';
+import { activeNotebook } from './utils.js';
+
+const /** {HTMLElement} */ $sidebarList = document.querySelector( '[data-sidebar-list]' );
+const /** {HTMLElement} */ $notePanelTitle = document.querySelector( '[data-note-panel-title]' );
+
+/**
+   * The client object manages interaction with the UI.
+   * @namespace
+   * @property {Object} notebook
+   * @property {Object} note
+   */
+export const client = {
+
+  notebook: {
+    /**
+     * @param {Object} notebookData
+     */
+    create ( notebookData )
+    {
+      const /** {HTMLElement} */ $navItem = NavItem( notebookData.id, notebookData.name );
+      $sidebarList.appendChild( $navItem );
+      activeNotebook.call( $navItem );
+      $notePanelTitle.textContent = notebookData.name;
+    },
+
+
+    /**
+     * @param {Array<Object>} notebookList
+     */
+    read ( notebookList )
+    {
+      notebookList.forEach( ( notebookData, index ) =>
+      {
+        const /** {HTMLElement} */ $navItem = NavItem( notebookData.id, notebookData.name );
+        if ( index === 0 )
+        {
+          activeNotebook.call( $navItem );
+          $notePanelTitle.textContent = notebookData.name;
+        }
+        $sidebarList.appendChild( $navItem );
+      } );
+    },
+  }
+
+}
