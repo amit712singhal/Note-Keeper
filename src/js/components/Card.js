6e92dbe875e09ef580e7244069f76e767a4903e1
amit712singhal <rakshit.singhal712@gmail.com>
2024-10-23 21:00:27 +0530

    refactor: Add Card component for displaying note details in app.js

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -3,3 +3,43 @@
  */
 
 'use strict';
+
+import { Tooltip } from "./Tooltip.js";
+import { getRelativeTime } from "../utils.js";
+
+/**
+ * @param {Object} noteData
+ * @returns {HTMLElement}
+ */
+export const Card = function ( noteData )
+{
+  const { id, title, text, postedOn, notebookId } = noteData;
+  const /** {HTMLElement} */ $card = document.createElement( 'div' );
+  $card.classList.add( 'card' );
+  $card.setAttribute( 'data-note', id );
+
+  $card.innerHTML = `
+          <h3 class="card-title text-title-medium">${ title }</h3>
+
+          <p class="card-text text-body-large">
+            ${ text }
+          </p>
+
+          <div class="wrapper">
+
+            <span class="card-time text-label-large">${ getRelativeTime( postedOn ) }</span>
+
+            <button class="icon-btn large" aria-label="Delete note" data-tooltip="Delete note">
+              <span class="material-symbols-rounded" aria-hidden="true">delete</span>
+              <div class="state-layer"></div>
+            </button>
+
+          </div>
+
+          <div class="state-layer"></div>
+  `;
+
+  Tooltip( $card.querySelector( '[data-tooltip]' ) );
+
+  return $card;
+}
