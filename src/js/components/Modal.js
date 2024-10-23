87d310a7746a85f95e34c80fc9912079401e3df8
amit712singhal <rakshit.singhal712@gmail.com>
2024-10-23 21:00:33 +0530

    refactor: Add NoteModal component for creating and editing notes

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -8,32 +8,113 @@ const /** {HTMLElement} */ $overlay = document.createElement( 'div' );
 $overlay.classList.add( 'overlay', 'modal-overlay' );
 
 /**
+ *
+ * @param {*} title
+ * @param {*} text
+ * @param {*} time
+ */
+const NoteModal = function ( title = 'Unitiled', text = 'Add your note ...', time = '' )
+{
+  const /** {HTMLElement} */ $modal = document.createElement( 'div' );
+  $modal.classList.add( 'modal' );
+  $modal.innerHTML = `
+        <button class="icon-btn large" aria-label="Close modal" data-close-btn>
+          <span class="material-symbols-rounded" aria-hidden="true">close</span>
+          <div class="state-layer"></div>
+        </button>
+
+        <input type="text" placeholder="Untitled" value="${ title }" class="modal-title text-title-medium"
+          data-note-field>
+
+        <textarea placeholder="Take a note..." class="modal-text text-body-large custom-scrollbar"
+          data-note-field>${ text }</textarea>
+
+        <div class="modal-footer">
+
+          <span class="time text-label-large">${ time }</span>
+
+          <button class="btn text" data-submit-btn>
+            <span class="text-label-large">Save</span>
+            <div class="state-layer"></div>
+          </button>
+
+        </div>
+  `;
+
+  const /** {HTMLElement} */ $submitBtn = $modal.querySelector( '[data-submit-btn]' );
+  $submitBtn.disabled = true;
+
+  const /** {HTMLElement} */[ $titleField, $textField ] = $modal.querySelectorAll( '[data-note-field]' );
+
+  const enableSubmitBtn = function ()
+  {
+    $submitBtn.disabled = !$titleField.value && $textField.value;
+  }
+
+  $textField.addEventListener( 'keyup', enableSubmitBtn );
+  $titleField.addEventListener( 'keyup', enableSubmitBtn );
+
+  // open note modal
+  const open = function ()
+  {
+    document.body.appendChild( $modal );
+    document.body.appendChild( $overlay );
+    $titleField.focus();
+  };
+
+  // close note modal
+  const close = function ()
+  {
+    document.body.removeChild( $modal );
+    document.body.removeChild( $overlay );
+  };
+
+  const /** {HTMLElement} */ $closeBtn = $modal.querySelector( '[data-close-btn]' );
+  $closeBtn.addEventListener( 'click', close );
+
+  /**
+   *
+   * @param {*} callback
+   */
+  const onSubmit = function ( callback )
+  {
+    $submitBtn.addEventListener( 'click', function ()
+    {
+      const /** {Object} */ noteData = {
+        title: $titleField.value,
+        text: $textField.value
+      }
+      callback( noteData );
+    } );
+  }
+
+  return { open, close, onSubmit };
+}
+
+/**
  * Creates a modal with a title and a message to confirm the delete operation
  *
  * @param {string} title
  * @returns {Object}
  */
-
 const DeleteConfirmModal = function ( title )
 {
   const /** {HTMLElement} */ $modal = document.createElement( 'div' );
   $modal.classList.add( 'modal' );
   $modal.innerHTML = `
-          <h3 class="modal-title text-title-medium">Are you sure you want to delete <strong>"${ title }"</strong>?</h3>
+    <h3 class="modal-title text-title-medium">Are you sure you want to delete <strong>"${ title }"</strong>?</h3>
 
-          <div class="modal-footer">
+    <div class="modal-footer">
+      <button class="btn text" data-action-btn="false">
+        <span class="text-label-large">Cancel</span>
+        <div class="state-layer"></div>
+      </button>
 
-          <button class="btn text" data-action-btn="false">
-            <span class="text-label-large">Cancel</span>
-            <div class="state-layer"></div>
-          </button>
-
-          <button class="btn fill" data-action-btn="true">
-            <span class="text-label-large">Delete</span>
-            <div class="state-layer"></div>
-          </button>
-
-        </div>
+      <button class="btn fill" data-action-btn="true">
+        <span class="text-label-large">Delete</span>
+        <div class="state-layer"></div>
+      </button>
+    </div>
   `;
 
   /**
@@ -43,7 +124,7 @@ const DeleteConfirmModal = function ( title )
   {
     document.body.appendChild( $modal );
     document.body.appendChild( $overlay );
-  }
+  };
 
   /**
    * Closes the delete confirm modal
@@ -52,24 +133,24 @@ const DeleteConfirmModal = function ( title )
   {
     document.body.removeChild( $modal );
     document.body.removeChild( $overlay );
-  }
+  };
 
-  const /** {Array<HTMLElement>} */ $actionBtns = $modal.querySelectorAll( '[data-action-btn]' );
+  const /** {NodeListOf<HTMLElement>} */ $actionBtns = $modal.querySelectorAll( '[data-action-btn]' );
+
   /**
-   * Handles the submission of the delete confirm modal
+   * Handles the submission of the delete confirmation
    * @param {Function} callback
    */
-
   const onSubmit = function ( callback )
   {
-    $actionBtns.forEach( $btn => $btn.addEventListener( 'click', function ( )
+    $actionBtns.forEach( $btn => $btn.addEventListener( 'click', function ()
     {
       const /** {boolean} */ isConfirm = this.dataset.actionBtn === 'true' ? true : false;
       callback( isConfirm );
     } ) );
-  }
+  };
 
   return { open, close, onSubmit };
-}
+};
 
-export { DeleteConfirmModal };
+export { DeleteConfirmModal, NoteModal };
